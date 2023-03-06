724e7dca00dcb46ec515e6bac1bda1cad12ff2be
victorMarkevich <viktor.markevich@faceit.com.ua>
2023-03-06 09:58:21 +0100

    Add allowed params into url

--- lib/seatable_ruby/row.rb
+++ lib/seatable_ruby/row.rb
@@ -3,15 +3,18 @@ require "net/http"
 
 module SeatableRuby
   class Row
-    attr_accessor :options, :access_data
+    attr_accessor :params, :access_data
+    # ALLOWED_PARAMS => [:table_name, :view_name, :convert_link_id, :order_by, :direction, :start, :limit]
 
-    def initialize(options = {})
+    def initialize(params = {})
       @access_data = Client.new.access_data
-      @options = options.merge(@access_data)
+      @params = params
     end
 
-    def get_table_rows
-      url = URI("https://cloud.seatable.io/dtable-server/api/v1/dtables/#{access_data['dtable_uuid']}/rows/?table_name=#{options[:table_name]}")
+    def list_rows
+      # TODO: add check for allowed params
+      url = URI("https://cloud.seatable.io/dtable-server/api/v1/dtables/#{access_data['dtable_uuid']}/rows")
+      url.query = URI.encode_www_form(params)
 
       https = Net::HTTP.new(url.host, url.port)
       https.use_ssl = true
@@ -19,8 +22,11 @@ module SeatableRuby
       request = Net::HTTP::Get.new(url)
       request["Authorization"] = "Token #{access_data['access_token']}"
       response = https.request(request)
-      SeatableRuby.parse(response.read_body)
+      data = SeatableRuby.parse(response.read_body)
+      p data
+      data
       # the response example here https://api.seatable.io/#528ae603-6dcc-4dc3-846f-a38974a4795d
     end
+
   end
 end
